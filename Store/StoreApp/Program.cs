49c9c30ea321f338a3ccce859fb2feebee8f8006
ProbisEren <metineren0061@gmail.com>
2026-07-28 12:38:39 +0300

    layout section of btk edu. is completed

--- Store/StoreApp/Program.cs
+++ Store/StoreApp/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddDbContext<RepositoryContext>(options => // ProductCOntroller
  
 var app = builder.Build();
 
+app.UseStaticFiles();
 app.UseHttpsRedirection();
 app.UseRouting();
 
